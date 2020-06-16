d5efe2ae69a3a265170a25fd8646c2fc7f015dc2
abdelrahman <abdelrahman.e@gate7.me>
2020-06-17 00:13:45 +0200

    Seeding Data

--- database/migrations/2020_06_16_141536_create_comments_table.php
+++ database/migrations/2020_06_16_141536_create_comments_table.php
@@ -16,6 +16,7 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->engine = "InnoDB";
             $table->bigIncrements('id');
+            $table->text('body');
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('post_id');
             $table->timestamps();
--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -11,6 +11,25 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        \App\Category::create([
+          'name'=>'html tutorials',
+          'slug'=>'html-tutorials',
+        ]);
+        \App\Category::create([
+          'name'=>'css tutorials',
+          'slug'=>'css-tutorials',
+        ]);
+        \App\Category::create([
+          'name'=>'javascript tutorials',
+          'slug'=>'javascript-tutorials',
+        ]);
+        \App\Category::create([
+          'name'=>'laravel tutorials',
+          'slug'=>'laravel-tutorials',
+        ]);
+        \App\Category::create([
+          'name'=>'vue js tutorials',
+          'slug'=>'vue-js-tutorials',
+        ]);
     }
 }
