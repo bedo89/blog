85b966796db614a02e2c7c293c1d82be9fc2dbdd
abdelrahman <abdelrahman.e@gate7.me>
2020-07-19 01:08:51 +0200

    form simple validation

--- resources/js/components/Register.vue
+++ resources/js/components/Register.vue
@@ -10,22 +10,35 @@
                 </div>
                 <div class="modal-body">
                     <div class="login-form">
-                        <form action="/examples/actions/confirmation.php" method="post">
+                        <form action="/examples/actions/confirmation.php" method="post" nonvalidate>
                             <h2 class="text-center">Create New Account</h2>
                             <div class="form-group">
-                                <input type="text" class="form-control" placeholder="firstname" required="required">
+                                <input type="text" class="form-control" v-model="firstname" placeholder="Firstname">
+                                <div class="text-danger" v-show="firstnameError">
+                                    ... This first name is too short !!
+                                </div>
                             </div>
                             <div class="form-group">
-                                <input type="text" class="form-control" placeholder="lastname" required="required">
+                                <input type="text" class="form-control" v-model="lastname" placeholder="Lastname">
+                                <div class="text-danger" v-show="lastnameError">
+                                    ... This last name is too short !!
+                                </div>
                             </div>
                             <div class="form-group">
-                                <input type="email" class="form-control" placeholder="email" required="required">
+                                <input type="email" class="form-control" v-model="email" placeholder="Email">
+                                <div class="text-danger" v-show="emailError">
+                                    ... This email is not valid !!
+                                </div>
                             </div>
                             <div class="form-group">
-                                <input type="password" class="form-control" placeholder="Password" required="required">
+                                <input type="password" class="form-control" v-model="password" placeholder="Password">
+                                <div class="text-danger" v-show="passwordError">
+                                    ... This password is too short !!
+                                </div>
                             </div>
+
                             <div class="form-group">
-                                <button type="submit" class="btn btn-primary btn-block">Create Account</button>
+                                <button type="submit" class="btn btn-primary btn-block" :disabled="!isValidForm">Create Account</button>
                             </div>
                             <div class="clearfix">
                                 <a href="#" class="pull-right">Forgot Password?</a>
@@ -43,6 +56,32 @@
 
 <script>
     export default {
-        name: "Register"
+        name: "Register",
+        data(){
+            return {
+                firstname : '',
+                lastname : '',
+                password : '',
+                email : ''
+            }
+        },
+        computed:{
+            firstnameError(){
+                return this.firstname.length > 0 && this.firstname.length < 4;
+            },
+            lastnameError(){
+                return this.lastname.length > 0 && this.lastname.length < 4;
+            },
+            emailError(){
+                return this.email.length > 0 && !(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.email));
+            },
+            passwordError(){
+                return this.password.length > 0 && this.password.length < 7;
+            },
+            isValidForm(){
+                return this.firstname.length > 4 && this.lastname.length > 4
+                    && (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.email)) && this.password.length > 7;
+            }
+        }
     }
 </script>
