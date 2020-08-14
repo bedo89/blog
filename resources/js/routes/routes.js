83a63b8d4d88df5666a8fc9a54a05d60acba562d
abdo_esam89 <elmoo4everyahoo.com@gmail.com>
2020-08-14 10:43:53 +0200

    change admin template

--- resources/js/routes/routes.js
+++ resources/js/routes/routes.js
@@ -6,11 +6,13 @@ Vue.use(VueRouter);
 import Posts from "../components/Posts.vue";
 import PostDetails from "../components/PostDetails.vue"
 import CategoryPosts from "../components/CategoryPosts.vue"
+import AdminIndex from "../components/admin/AdminIndex.vue"
 
 const routes = [
     {path: '/', component: Posts, name: Posts},
     {path: '/post/:slug', component: PostDetails, name: PostDetails},
-    {path: '/category/:slug/posts', component: CategoryPosts, name: CategoryPosts}
+    {path: '/category/:slug/posts', component: CategoryPosts, name: CategoryPosts},
+    {path: '/admin', component: AdminIndex, name: AdminIndex}
 ];
 
 const router = new VueRouter({
